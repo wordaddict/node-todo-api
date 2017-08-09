7ee8ac8675f5fb4e5c494d203ab72383f3a3fc5e
Adeyinka Micheal <madeyinka6@gmail.com>
2017-08-10 00:50:25 +0100

    Server modified with the routes working properly

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+var port = 3000;
+app.listen(port, () => {
+  console.log(`Started on port ${3000}`)
+});
